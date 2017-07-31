e82d051dd18520e9ce0b63c71578a0b9a7983af4
Shawon Kanji <shawonkanji46@gmail.com>
2017-07-31 23:51:06 +0530

    init rollback

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # rollbackjs
+
+async = require('async');
+rollback = require('./modules/util/rollback')(async);
+
+rollback.parallel([{
+    transaction: (cb) => {
+        setTimeout(() => {
+            cb(null, 1);
+        }, 100);
+    },
+    rollback: (num, cb) => {
+        setTimeout(() => {
+            cb(null, "rollback - " + num);
+        }, 100);
+    }
+}, {
+    transaction: (cb) => {
+        setTimeout(() => {
+            cb(null, 2);
+        }, 200);
+    },
+    rollback: (num, cb) => {
+        setTimeout(() => {
+            cb(null, "rollback - " + num);
+        }, 20);
+    }
+}, {
+    transaction: (cb) => {
+        setTimeout(() => {
+            cb(3);
+        }, 500);
+    },
+    rollback: (num, cb) => {
+        setTimeout(() => {
+            cb(null, "rollback - " + num);
+        }, 500);
+    }
+}], (err, data) => {
+    console.log("final", err, data);
+});
---
+++ rollback.js
@@ -0,0 +1,29 @@
+class Rollback {
+
+    constructor(async) {
+        this.async = async;
+    }
+    parallel(functionPipeline, callback) {
+        var transactionStack = functionPipeline.map(pipeLine => pipeLine.transaction);
+        var rollbackStack = functionPipeline.map(pipeLine => pipeLine.rollback);
+        this.async.parallel(transactionStack, (err, result) => {
+            if (err) {
+                Promise.all(rollbackStack.map((rb, i) => new Promise((resolve, reject) => {
+                        rb(result[i], (e, r) => {
+                            !!e ? reject(e) : resolve(r)
+                        });
+                    })))
+                    .then(r => callback(null, r))
+                    .catch(e => callback(e));
+            } else {
+                callback(null, result);
+            }
+        });
+
+
+    };
+};
+
+module.exports = (async) => {
+    return new Rollback(async);
+}
